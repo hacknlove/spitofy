abb7734ce29840a21d6845390f2473ada7ddea5d
hacknlove <antonio@hacknlove.org>
2023-12-14 15:21:05 +0100

    sync

--- src/components/Menu/Cart/CartContent.tsx
+++ src/components/Menu/Cart/CartContent.tsx
@@ -1,45 +1,40 @@
-import { createEffect, createSignal, For } from "solid-js"
+import { createSignal, For } from "solid-js";
 import { ProductItem } from "./ProductItem";
 
 type ProductItem = {
-    image: string;
-    name: string;
-    slug: string;
-    price: string;
-    size: string;  
-    amount: number; 
-    id: string;     
-  };
+  image: string;
+  name: string;
+  slug: string;
+  price: string;
+  size: string;
+  amount: number;
+  id: string;
+};
 
+declare global {
   interface Window {
     addToCart: (product: ProductItem) => void;
     removeFromCart: (product: ProductItem) => void;
   }
+}
 
-  declare global {
-    interface Window {
-      addToCart: (product: ProductItem) => void;
-      removeFromCart: (product: ProductItem) => void;
-    }
-  }
+export default function CartContent() {
+  const [cart, setCart] = createSignal<ProductItem[]>([]);
 
-export default function CartContent () {
-    const [cart, setCart] = createSignal<ProductItem[]>([])
+  const Cart = document.getElementById("Cart") as HTMLDivElement;
 
-    const Cart = document.getElementById("Cart") as HTMLDivElement;
+  window.addToCart = (product) => {
+    setCart([...cart(), product]);
 
-    window.addToCart = (product) => {
-        setCart([...cart(), product])
+    Cart.dataset.items = cart().length.toString();
+  };
+  window.removeFromCart = (product) => {
+    setCart(cart().filter((p) => p !== product));
+  };
 
-        Cart.dataset.items = cart().length.toString()
-    }
-    window.removeFromCart = (product) => {
-        setCart(cart().filter((p) => p !== product))
-    }
-
-    return (
-        <div>
-            <For each={cart()}>{(product) => <ProductItem product={product} />}</For>
-        </div>
-    )
+  return (
+    <div>
+      <For each={cart()}>{(product) => <ProductItem product={product} />}</For>
+    </div>
+  );
 }
--- src/components/Products/addToCart.ts
+++ src/components/Products/addToCart.ts
@@ -1,12 +1,46 @@
+type ProductKind = {
+  image: string;
+  name: string;
+  slug: string;
+  price: string;
+  sizes: [string];
+  stock: Record<string, number>;
+};
+declare global {
+  interface Window {
+    productsBySlug: Record<string, ProductKind>;
+  }
+}
+
 function outOfStock() {
+  console.log(this);
   setTimeout(() => {
     this.querySelector(".outOfStock").style.display = "block";
     this.querySelector("button").style.display = "none";
   }, 200);
 }
+
+function addToCart() {
+  const productElement = this.closest("[data-slug]");
+
+  const product = window.productsBySlug[productElement.dataset.slug];
+
+  const size = productElement.querySelector("input:checked")?.value;
+
+  if (size && product.stock[size] <= 0) {
+    outOfStock.call(productElement);
+  }
+}
+
 document.addEventListener("astro:page-load", () => {
-  document.querySelectorAll("#Products>div").forEach((button) => {
-    button.addEventListener("click", outOfStock);
+  document.querySelectorAll("#Products button").forEach((button) => {
+    button.addEventListener("click", addToCart);
+  });
+
+  document.querySelectorAll("input").forEach((input) => {
+    input.addEventListener("change", function () {
+      console.log(this.value, this.checked);
+    });
   });
 });
 
