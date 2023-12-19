3b343b69515348cd3dd93459bc0eacead0f803d8
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-12-19 21:16:47 +0100

    lots of stuff

--- src/js/reactions.ts
+++ src/js/reactions.ts
@@ -38,3 +38,87 @@ export function launch(id) {
     newSvg.remove();
   }, speed * 1000);
 }
+
+function addReactionToWaveform(reaction) {
+  const icon = icons[reaction.reaction] ?? (icons.heart as HTMLImageElement);
+  const newSvg = icon.cloneNode(true) as HTMLImageElement;
+  newSvg.removeAttribute("id");
+
+  newSvg.classList.add("reactionIcon");
+
+  newSvg.style.setProperty(
+    "--left",
+    `${(reaction.currentTime / audio.duration) * 100}%`,
+  );
+
+  const waveFormProgress = document.getElementById(
+    "WaveFormProgress",
+  ) as HTMLDivElement;
+
+  waveFormProgress.appendChild(newSvg);
+}
+
+let reactions = [] as { reaction: string; currentTime: number }[];
+let loaded = false;
+let disableReactions = false;
+let i = 0;
+
+async function loadReactions() {
+  console.log("loading reactions");
+  if (loaded) {
+    return;
+  }
+  loaded = true;
+
+  reactions = await fetch(`/getReactions?track=${audio.dataset.slug}`).then(
+    (res) => res.json(),
+  );
+  reactions.sort((a, b) => a.currentTime - b.currentTime);
+
+  for (const reaction of reactions) {
+    addReactionToWaveform(reaction);
+  }
+
+  if (audio.currentTime) {
+    seeked();
+  }
+}
+
+function timeupdate() {
+  if (disableReactions) {
+    return;
+  }
+  if (audio.currentTime > reactions[i]?.currentTime) {
+    launch(reactions[i].reaction);
+    i++;
+  }
+}
+
+function seeked() {
+  i = 0;
+  while (audio.currentTime > reactions[i]?.currentTime) {
+    i++;
+  }
+  disableReactions = false;
+}
+
+function seeking() {
+  disableReactions = true;
+}
+
+function ended() {
+  console.log("ended");
+  disableReactions = false;
+  i = 0;
+  reactions = [];
+  document.querySelectorAll("#WaveFormProgress .reactionIcon").forEach((el) => {
+    el.remove();
+  });
+  loaded = false;
+}
+
+audio.addEventListener("play", loadReactions);
+audio.addEventListener("timeupdate", timeupdate);
+audio.addEventListener("seeked", seeked);
+audio.addEventListener("seeking", seeking);
+audio.addEventListener("ended", ended);
