347c38e6f45842fabae370949f7f039a1743082a
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-12-19 19:03:55 +0100

    endpoints to get and set the reactions

---
+++ functions/getReactions.js
@@ -0,0 +1,41 @@
+const allowedTracks = new Set([
+  "besame",
+  "fuck off baby",
+  "los muros más altos",
+  "soundtrack",
+  "traia",
+  "we will never die",
+]);
+
+async function getReactions(context, params, reactions = []) {
+  const data = await context.env.REACTIONS.list(params);
+
+  for (const key of data.keys) {
+    reactions.push(key.metadata);
+  }
+
+  if (data.list_complete) {
+    return reactions;
+  }
+
+  params.cursor = data.cursor;
+
+  return getReactions(context, params, reactions);
+}
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const track = url.searchParams.get("track");
+
+  if (!allowedTracks.has(track)) {
+    return new Response("Track not allowed", { status: 403 });
+  }
+
+  const reactions = await getReactions(context, { prefix: track });
+
+  return new Response(JSON.stringify(reactions), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
---
+++ functions/sendReaction.js
@@ -0,0 +1,36 @@
+const trackDurations = {
+  besame: 245.333333,
+  "fuck off baby": 272.013025,
+  "los muros más altos": 164.440775,
+  soundtrack: 273.6,
+  traia: 312.827596,
+  "we will never die": 155.533025,
+};
+
+export async function onRequest(context) {
+  if (context.request.method !== "POST") {
+    return new Response("Only POST requests are accepted", { status: 405 });
+  }
+
+  const data = await context.request.json();
+
+  if (!trackDurations[data.track]) {
+    return new Response("Track not allowed", { status: 403 });
+  }
+
+  if (data.currentTime < 0 || data.currentTime > trackDurations[data.track]) {
+    return new Response("Invalid time", { status: 403 });
+  }
+
+  context.waitUntil(
+    context.env.REACTIONS.put(`${data.track}/${crypto.randomUUID()}`, "", {
+      expirationTtl: 365.25 * 24 * 60 * 60,
+      metadata: {
+        reaction: data.reaction,
+        currentTime: data.currentTime,
+      },
+    }),
+  );
+
+  return new Response();
+}
